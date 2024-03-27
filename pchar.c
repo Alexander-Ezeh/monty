ccc5428fafc2ce6238f1eacceb47db196aef4a1f
Alexander Ikennaefuna Ezeh <122819061+Alexander-Ezeh@users.noreply.github.com>
2024-03-27 18:55:38 +0100

    pchar.c

--- pchar.c
+++ pchar.c
@@ -1,11 +1,12 @@
 #include "monty.h"
 /**
- * f_pchar - prints the char at the top of the stack,
- * followed by a new line
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
+ * f_pchar - Prints the character at the top of the stack followed by a new line.
+ * @head: Pointer to the head of the stack.
+ * @counter: Line number.
+ *
+ * Return: No return.
+ */
+
 void f_pchar(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
