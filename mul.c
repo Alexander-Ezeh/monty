464c5c6942a5ba08d65d0826523fbeabbe47e1db
Alexander Ikennaefuna Ezeh <122819061+Alexander-Ezeh@users.noreply.github.com>
2024-03-27 18:52:27 +0100

    mul.c

--- mul.c
+++ mul.c
@@ -1,10 +1,12 @@
 #include "monty.h"
 /**
- * f_mul - multiplies the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
+ * f_mul - Multiplies the top two elements of the stack.
+ * @head: Pointer to the head of the stack.
+ * @counter: Line number.
+ *
+ * Return: No return.
+ */
+
 void f_mul(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
